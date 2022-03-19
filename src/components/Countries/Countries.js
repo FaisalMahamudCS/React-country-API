e52a178c565d8c2715a88cf129ef6af00e5b5455
FaisalMahamudCS <fmfahimcse@gmail.com>
2022-03-19 13:52:30 +0600

    Add

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 const Countries = () => {
     const [countries,setCountries]=useState([])
     useEffect(() =>{
@@ -9,10 +10,10 @@ const Countries = () => {
         .then(data=>setCountries(data))
       },[])
     return (
-        <div>
-            <h2>Countries in others</h2>
+        <div className='countries-container'>
+    
             {
-                countries.map(country => <Country country={country} name={country.name.common}></Country>)
+                countries.map(country => <Country country={country} name={country.name.common} key={country.cca3} ></Country>)
             }
         </div>
     );
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Country.css'
 
 const Country = (props) => {
+    const {flags}=props.country;
     return (
-        <div>
+        <div className='country'>
             <h3>{props.name}</h3>
+            <img src={flags.png} alt="" srcset="" />
+            <p>Area:{props.country.area}</p>
+            <p>Region:{props.country.region}</p>
         </div>
     );
 };
